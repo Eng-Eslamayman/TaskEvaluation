bd2bc700feee143c0e016a67c5d36ed4e1eb4230
eslam <eslam@ESLAMAYMAN>
2024-03-29 02:30:28 +0200

    - rename TaskEvaluation.AccessLayer to TaskEvaluation.Infrastructure - add Fluent Api Configurations Classes

---
+++ TaskEvaluation.Infrastructure/Configurations/AssignmentConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskEvaluation.Core.Entities.Business;
+
+namespace TaskEvaluation.Infrastructure.Configurations
+{
+    public class AssignmentConfiguration : IEntityTypeCounfiguration<Assignment>
+    {
+    }
+}
