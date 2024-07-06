936d9a97bb10c96e340162057237004c2e7cf917
mahmoudtealb <114764899+mahmoudtealb@users.noreply.github.com>
2024-07-06 16:47:29 +0200

    seed data in ApllicationDbContext and add migration name ="SeedData" , updatedatebase

--- TaskEvaluation.Infrastructure/Data/ApplicationDbContext.cs
+++ TaskEvaluation.Infrastructure/Data/ApplicationDbContext.cs
@@ -10,8 +10,8 @@ using TaskEvaluation.Core.Entities.Business;
 
 namespace TaskEvaluation.Infrastructure.Data
 {
-    public class ApplicationDbContext: IdentityDbContext
-	{
+    public class ApplicationDbContext : IdentityDbContext
+    {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
              : base(options)
         {
@@ -19,8 +19,8 @@ namespace TaskEvaluation.Infrastructure.Data
 
         public DbSet<Assignment> Assignments { get; set; }
         public DbSet<Course> Courses { get; set; }
-        public DbSet<EvaluationGrade> EvalutionGrades { get; set;}
-        public DbSet<Group> Groups { get; set; }
+        public DbSet<EvaluationGrade> EvalutionGrades { get; set; }
+       public DbSet<Group> Groups { get; set; }
         public DbSet<Solution> Solutions { get; set; }
         public DbSet<Student> Students { get; set; }
 
@@ -30,11 +30,31 @@ namespace TaskEvaluation.Infrastructure.Data
 
             base.OnModelCreating(modelBuilder);
 
-			foreach (var foreignKey in modelBuilder.Model.GetEntityTypes()
-		    .SelectMany(e => e.GetForeignKeys()))
-			{
-				foreignKey.DeleteBehavior = DeleteBehavior.SetNull;
-			}
-		}
+            
+            foreach (var foreignKey in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetForeignKeys()))
+            {
+                if (foreignKey.DeleteBehavior == DeleteBehavior.SetNull)
+                {
+                    foreignKey.DeleteBehavior = DeleteBehavior.Restrict;
+                }
+            }
+
+                modelBuilder.Entity<Course>().HasData(
+                new Course { Id = 1, Title = "Front End", IsCompleted = false },
+                new Course { Id = 2, Title = "Back End", IsCompleted = false }
+            );
+
+            modelBuilder.Entity<Group>().HasData(
+                new Group { Id = 1, Title = "A", CourseId = 1 },
+                new Group { Id = 2, Title = "B", CourseId = 2 }
+            );
+
+            modelBuilder.Entity<EvaluationGrade>().HasData(
+                new EvaluationGrade { Id = 1, Grade = "Good" },
+                new EvaluationGrade { Id = 2, Grade = "Very good" },
+                new EvaluationGrade { Id = 3, Grade = "Excellent" }
+            );
+        }
     }
 }
